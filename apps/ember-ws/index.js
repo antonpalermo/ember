0b2b354cd84a1887ef3e7869320d36e99ba85bdf
Anton Palermo <anton.palermo+github@dragonpay.ph>
2024-12-22 19:59:10 +0800

    refac: update ws setup

--- apps/ember-ws/index.js
+++ apps/ember-ws/index.js
@@ -1,10 +1,29 @@
-const express = require("express");
-
-const app = express();
+const WebSocket = require("ws");
 const port = 8080;
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ server: "ok" });
+const wss = new WebSocket.Server({ port }, () => {
+  console.log("👂 for connection on", `ws://localhost:${port}`);
 });
 
-app.listen(port, () => console.log(`http://localhost:${port}`));
+wss.on("connection", ws => {
+  console.log("✅ client connected");
+
+  ws.on("message", msg => {
+    if (Buffer.isBuffer(msg)) {
+      const message = JSON.parse(msg.toString());
+
+      switch (message.event) {
+        case "send":
+          console.log("sending triggered");
+          break;
+        case "drop":
+          console.log("droping triggered");
+          break;
+      }
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("❌ client disconnected");
+  });
+});
